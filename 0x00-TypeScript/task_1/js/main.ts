990d3f9cba984252d9e61e5775112fbb24b42b18
jzamora5 <jzamora_5@yahoo.com>
2021-02-08 11:06:06 -0500

    Student Class

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -44,3 +44,32 @@ interface printTeacherFunction {
 // let x: printTeacherFunction = printTeacher;
 
 // console.log(x("Larry", "Hudson"));
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
