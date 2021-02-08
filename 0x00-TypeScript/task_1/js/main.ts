0f108ff89e79f9fc9193b206e15525be556d873b
jzamora5 <jzamora_5@yahoo.com>
2021-02-08 10:35:45 -0500

    Eslint Fix

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -3,9 +3,9 @@ interface Teacher {
   lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
-  /* eslint-disable @typescript-eslint/no-explicit-any*/
+  /* eslint-disable @typescript-eslint/no-explicit-any */
   [key: string]: any;
-  /* eslint-enable @typescript-eslint/no-explicit-any*/
+  /* eslint-enable @typescript-eslint/no-explicit-any */
 }
 
 // const teacher3: Teacher = {
@@ -22,11 +22,25 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-const director1: Directors = {
-  firstName: "John",
-  lastName: "Doe",
-  location: "London",
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
-console.log(director1);
+// const director1: Directors = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   location: "London",
+//   fullTimeEmployee: true,
+//   numberOfReports: 17,
+// };
+// console.log(director1);
+
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+/* eslint-disable @typescript-eslint/class-name-casing */
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+/* eslint-enable @typescript-eslint/class-name-casing */
+
+// let x: printTeacherFunction = printTeacher;
+
+// console.log(x("Larry", "Hudson"));
