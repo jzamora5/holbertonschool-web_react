90c0c16c40f1908e2c83951054dac3497f523bbb
jzamora5 <jzamora_5@yahoo.com>
2021-02-09 11:42:19 -0500

    Refere path with slash

--- 0x00-TypeScript/task_3/js/main.ts
+++ 0x00-TypeScript/task_3/js/main.ts
@@ -1,10 +1,10 @@
-/// <reference path="crud.d.ts" />
+/// <reference path="./crud.d.ts" />
 import { RowID, RowElement } from "./interface";
 import * as CRUD from "./crud";
 
 const row: RowElement = {
-  firstName: 'Guilaume',
-  lastName: 'Salva',
+  firstName: "Guillaume",
+  lastName: "Salva",
 };
 
 const newRowID: RowID = CRUD.insertRow(row);
