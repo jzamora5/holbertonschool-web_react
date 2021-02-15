41b3ac8130b6cb4042a2dc91bc954573d9b49090
jzamora5 <jzamora_5@yahoo.com>
2021-02-15 18:03:56 -0500

    Create React tests

--- 0x02-react_intro/task_3/dashboard/src/utils.test.js
+++ 0x02-react_intro/task_3/dashboard/src/utils.test.js
@@ -1,11 +1,10 @@
-import { strict as assert } from "assert";
 import { getFullYear, getFooterCopy, getLatestNotification } from "./utils";
 
 describe("utils_tests", function () {
   describe("getFullYear", function () {
     it("should return current year", function () {
       const year = getFullYear();
-      assert.equal(year, new Date().getFullYear());
+      expect(year).toEqual(new Date().getFullYear());
     });
   });
   describe("getFooterCopy", function () {
@@ -14,18 +13,17 @@ describe("utils_tests", function () {
 
     it("Should return true message", function () {
       const msg = getFooterCopy(true);
-      assert.equal(msg, trueMsg);
+      expect(msg).toEqual(trueMsg);
     });
     it("Should return false message", function () {
       const msg = getFooterCopy(false);
-      assert.equal(msg, falseMsg);
+      expect(msg).toEqual(falseMsg);
     });
   });
   describe("getLatestNotification", function () {
     it("shold return correct string element", function () {
       const element = "<strong>Urgent requirement</strong> - complete by EOD";
-
-      assert.equal(getLatestNotification(), element);
+      expect(getLatestNotification()).toEqual(element);
     });
   });
 });
