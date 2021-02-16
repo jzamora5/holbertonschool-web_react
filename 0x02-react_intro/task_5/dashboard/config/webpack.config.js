a059aa4548f32ce9910357d3bd2143931bb589b5
jzamora5 <jzamora_5@yahoo.com>
2021-02-16 14:31:07 -0500

    Testing

--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   mode: "development",
   devtool: "inline-source-map",
-  entry: "/src/index.js",
+  entry: "./src/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve("./dist"),
@@ -18,9 +18,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        loader: "babel-loader",
       },
       {
         test: /\.css$/i,
--- 0x02-react_intro/task_5/dashboard/src/Notifications/Notifications.test.js
+++ 0x02-react_intro/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -1,7 +1,8 @@
 import { shallow } from "enzyme";
+import React from "react";
 import Notifications from "./Notifications";
 
-describe("<App />", () => {
+describe("<Notifications />", () => {
   it("Notifications renders without crashing", () => {
     const wrapper = shallow(<Notifications />);
     expect(wrapper.exists());
