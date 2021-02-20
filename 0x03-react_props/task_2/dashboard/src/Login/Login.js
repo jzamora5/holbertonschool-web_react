079b87f74df032d1ff31a3669682b15a7d2d28dd
jzamora5 <jzamora_5@yahoo.com>
2021-02-19 20:15:57 -0500

    Add colors to inputs

--- 0x03-react_props/task_2/dashboard/src/Login/Login.js
+++ 0x03-react_props/task_2/dashboard/src/Login/Login.js
@@ -5,9 +5,9 @@ function Login() {
   return (
     <div className="login">
       <p>Login to access the full dashboard</p>
-      <label htmlFor="email">Email</label>
+      <label htmlFor="email">Email:</label>
       <input type="email" id="email" />
-      <label htmlFor="password">Password</label>
+      <label htmlFor="password">Password:</label>
       <input type="password" id="password" />
       <button>OK</button>
     </div>
