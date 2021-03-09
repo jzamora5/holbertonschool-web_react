478139650043d1167079e0016679bd9054e394af
jzamora5 <jzamora_5@yahoo.com>
2021-03-09 18:19:49 -0500

    Make Notifications take all screen in responsive

--- 0x05-React_inline_styling/task_4/dashboard/src/Notifications/Notifications.js
+++ 0x05-React_inline_styling/task_4/dashboard/src/Notifications/Notifications.js
@@ -176,6 +176,10 @@ const styles = StyleSheet.create({
         border: "none",
         // animationFillMode: "forwards",
       },
+      position: "absolute",
+      background: "white",
+      height: "110vh",
+      width: "100vw",
     },
   },
 
