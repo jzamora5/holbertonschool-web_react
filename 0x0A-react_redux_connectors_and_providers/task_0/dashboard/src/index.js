40b6a0e8591619ba2f38839ec1afadb9f2055835
jzamora5 <jzamora_5@yahoo.com>
2021-03-30 19:17:29 -0500

    Fix state not being Map

--- 0x0A-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ 0x0A-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -4,8 +4,9 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import App from "./App/App";
 import uiReducer, { initialState } from "./reducers/uiReducer";
+import { Map } from "immutable";
 
-const store = createStore(uiReducer, initialState);
+const store = createStore(uiReducer, Map(initialState));
 
 ReactDOM.render(
   <React.StrictMode>
